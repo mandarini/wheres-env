e6564f827303fac4b6545797c2ac3d3061ae1674
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-11-14 16:20:09 +0200

    removed apps

--- apps/ngapp-e2e/src/e2e/app.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('ngapp', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary'));
-  it('should render the component', () => {
-    cy.get('sb-env-nx-root').should('exist');
-  });
-});
--- apps/ngapp-e2e/src/e2e/nx-welcome.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('ngapp', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=nxwelcomecomponent--primary'));
-  it('should render the component', () => {
-    cy.get('sb-env-nx-nx-welcome').should('exist');
-  });
-});
